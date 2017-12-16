8aba0913101a0f4ea3f9a755209d9d211b1447e4
James Bell <jtb2013@gmail.com>
2017-12-16 15:53:29 -0600

    Now using AltSoftSerial library

--- arduino_controller/arduino_controller.ino
+++ arduino_controller/arduino_controller.ino
@@ -1,17 +1,18 @@
 #include <SoftwareSerial.h>
 #include <XBee.h>
-SoftwareSerial roboSerial = SoftwareSerial(10, 11);
+#include <AltSoftSerial.h>
 SoftwareSerial xbeeSerial = SoftwareSerial(12, 13);
+AltSoftSerial roboSerial;
 XBee xbee = XBee();
+AltSoftSerial altser;
 Rx16Response rx16 = Rx16Response();
-int time;
+long time;
 bool read = false;//past tense;
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   roboSerial.begin(9600);
   xbeeSerial.begin(9600);
-  xbeeSerial.listen();
-  roboSerial.listen();
+  roboSerial.listen() ;
   xbee.setSerial(xbeeSerial);
   time=millis();
 }
@@ -21,56 +22,72 @@ void loop() {
 //    String msg=Serial.readString();
 //    if(msg.length()>0){
 //      Serial.println("Sent: "+msg);
-//      serial.println(msg);
+//      roboSerial.println(msg);
 //    }
 //  }
-//  while(serial.available()){
+//  while(roboSerial.available()){
 ////  char c=serial.read();
 ////  Serial.println(c);
+//    
 //    Serial.println("Received Message");
-//    Serial.println(serial.readString());
+//    String msg=roboSerial.readString();
+//    for(int i=0;i<msg.length();i++){
+//        Serial.print(msg.charAt(i),HEX);
+//        Serial.print(' ');
+//    }
+//    Serial.println();
+//    Serial.println(msg);
 //  }
+  
   xbee.readPacket();
   if (xbee.getResponse().isAvailable()) {
     read=true;
     uint8_t* data = xbee.getResponse().getFrameData();
-    Serial.println(xbee.getResponse().getFrameDataLength());
-    Serial.println("Received Frame");
-    for (int i=0;i<18;i++){
-      Serial.print(data[i],HEX);
-      Serial.print(" ");
-      roboSerial.write(data[i]);
+    int len=xbee.getResponse().getFrameDataLength();
+    if(len>0){
+      Serial.println(len);
+      Serial.println("Received Frame");
+      char command[len-4];
+      for (int i=4;i<len;i++){//first 4 are frame id, 16 bit address, and option byte
+
+        command[i-4]=data[i];
+      }
+      roboSerial.println(command);
+      Serial.println(command);
     }
-    roboSerial.print("\r");
-//    Serial.println();
-//    for (int i = 1; i < 5; i++) {
-//      Serial.println(*((float*)&data[i*4-2]));
-//    }
-//    ex[0] = *((float*)&data[0*4+2]);//x error
-//    ey[0] =*((float*)&data[1*4+2]);//y error
-//    theta = *((float*)&data[2*4+2]);//rotation
-//    eTheta[0] = *((float*)&data[3*4+2]);//rotation error
   }
-  //updateWheels();
-  if(read){
+ // roboSerial.listen();
+  long time_listen=millis();
+  while(read&&(millis()-time_listen<100)){//perhaps change to while(read&&time_waiting<10) or something
+    roboSerial.listen();
     String dataString="";
     while(roboSerial.available()){
       dataString=roboSerial.readString();
     }
     if(dataString.length()>0){
+      Serial.println(dataString);
       read=false;
       uint8_t data[dataString.length()];
       for(int i=0;i<dataString.length();i++){
         data[i]=dataString.charAt(i);
+        Serial.print(data[i],HEX);
+        Serial.print(' ');
       }
+      Serial.println();
       Tx16Request tx = Tx16Request(0xFFFD,data,dataString.length());
       xbee.send(tx);
     }
-    
   }
-  if(millis()-time>1000){
+  read=false;
+  xbeeSerial.listen();
+  if(millis()-time>5000){
     time=millis();
-    Tx16Request tx = Tx16Request(0xFFFD,"ping",4);
+    const char* dataStr="ping";
+    char data[sizeof dataStr];
+    strcpy(data,dataStr);
+    Tx16Request tx = Tx16Request(0xFFFD,(uint8_t*)data,10);
     xbee.send(tx);
+//    Serial.println("Pinged");
+//    roboSerial.println("!g 400");
   }
 }
